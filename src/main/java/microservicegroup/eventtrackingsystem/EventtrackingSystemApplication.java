bc5dff8d940c8f7ccb9e05d37ec2cf7a6e0fa81d
sbahadirersoz <115153941+sbahadirersoz@users.noreply.github.com>
2024-05-05 19:53:56 +0300

    api for eventtrackingSystem

---
+++ src/main/java/microservicegroup/eventtrackingsystem/EventtrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package microservicegroup.eventtrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventtrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventtrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microservicegroup/eventtrackingsystem/EventtrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicegroup.eventtrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventtrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
